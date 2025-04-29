b770c11a9f0ae6eae596134f93cc61a24b2fcad4
waly <65059215+wal8y@users.noreply.github.com>
2025-04-29 16:25:13 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'dart:io';
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/material.dart';
+import 'package:esyria/database_helper.dart';
 import 'package:esyria/pages/adding.dart';
 import 'package:esyria/pages/search.dart';
-import 'package:esyria/database_helper.dart';
-import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+import 'package:file_picker/file_picker.dart';
 
 void main() {
   sqfliteFfiInit();
@@ -22,7 +26,7 @@ class Esyria extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(
           seedColor: Colors.green,
           brightness: Brightness.dark,
-          background: Colors.black,
+          surface: Colors.black,
         ),
         useMaterial3: true,
       ),
@@ -53,6 +57,44 @@ class _MyHomePageState extends State<HomePage> {
       scaffoldMessenger.showSnackBar(
         SnackBar(content: Text('فشل التصدير: $e')),
       );
+    }
+  }
+
+  void _exportToExcel(BuildContext context) async {
+    final dbHelper = DatabaseHelper();
+    final scaffoldMessenger = ScaffoldMessenger.of(context);
+
+    try {
+      await dbHelper.exportToExcel();
+      scaffoldMessenger.showSnackBar(
+        const SnackBar(content: Text('تم تصدير قاعدة البيانات إلى Excel بنجاح')),
+      );
+    } catch (e) {
+      scaffoldMessenger.showSnackBar(
+        SnackBar(content: Text('فشل التصدير: $e')),
+      );
+    }
+  }
+
+  void _importFromExcel(BuildContext context) async {
+    final result = await FilePicker.platform.pickFiles(
+      type: FileType.custom,
+      allowedExtensions: ['xlsx'],
+    );
+    if (result != null && context.mounted) {
+      final dbHelper = DatabaseHelper();
+      final scaffoldMessenger = ScaffoldMessenger.of(context);
+      
+      try {
+        await dbHelper.importFromExcel(File(result.files.single.path!));
+        scaffoldMessenger.showSnackBar(
+          const SnackBar(content: Text('تم استيراد قاعدة البيانات من Excel بنجاح')),
+        );
+      } catch (e) {
+        scaffoldMessenger.showSnackBar(
+          SnackBar(content: Text('فشل الاستيراد: $e')),
+        );
+      }
     }
   }
 
@@ -118,6 +160,28 @@ class _MyHomePageState extends State<HomePage> {
                     onPressed: () => _exportDatabase(context),
                     icon: const Icon(Icons.import_export),
                     label: const Text("تصدير قاعدة البيانات"),
+                  ),
+                  const SizedBox(width: 20),
+                  ElevatedButton.icon(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue,
+                      padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+                      textStyle: const TextStyle(fontSize: 18),
+                    ),
+                    onPressed: () => _exportToExcel(context),
+                    icon: const Icon(Icons.import_export),
+                    label: const Text("تصدير إلى Excel"),
+                  ),
+                  const SizedBox(width: 20),
+                  ElevatedButton.icon(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue,
+                      padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+                      textStyle: const TextStyle(fontSize: 18),
+                    ),
+                    onPressed: () => _importFromExcel(context),
+                    icon: const Icon(Icons.upload),
+                    label: const Text("استيراد من Excel"),
                   ),
                 ],
               )
